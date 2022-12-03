86d1f2a88f6b54a3be17fbac09243624937e2b5b
Asif Sayyad <asifin90@gmail.com>
2022-12-03 22:26:34 +0530

    Create Sample1.cs

---
+++ Loops/For/Sample1.cs
@@ -0,0 +1,15 @@
+int i;  // i holds 1
+
+/*for(i=1; i <= 50; i++)
+{
+
+    Console.WriteLine(i);
+}*/
+
+for(i=20; i < 101; i++ )
+{
+    if( i % 2 == 0 )
+    {
+        Console.WriteLine(i);
+    }
+}
